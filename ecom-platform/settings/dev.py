3d6ae287c13a2f3eeecd8f7c45f16aaaf9015ec0
Miguel Angel Hernández Cortés <miguelhdezc12@gmail.com>
2024-02-28 21:54:36 -0600

    re-estructuración proyecto

---
+++ ecom-platform/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# TODO: CHECAR SECRET KEY
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-aaa&@&ej+194-qsnt)k01v*plu^_+u_!=)8gn#lrj&%^e=z4u&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
