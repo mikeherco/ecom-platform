985f14aa9d780ebdc591cd2628e1e8494763ecf5
Miguel Angel Hernández Cortés <miguelhdezc12@gmail.com>
2024-04-18 21:03:26 -0600

    modelos todo:

---
+++ paginas/models.py
@@ -0,0 +1,30 @@
+"""
+NavBar
+    - Logo -> imageField
+    - Color asociado al snippet de Clase color
+    - Titulo  (Obligatorio)
+    - mostrar_titulo y override cuando hay icono
+    - mostrar-buscador
+    - tipos paginas para la busqueda del buscador (snippet en streamblock)
+    - icono de antDesign!!
+
+
+SubMenu categos
+    - Categorias snippet independiente
+    - configurar mostrar en submenu en api
+
+Carrusel
+    - Checar Grupo Carrusel y Carrusel Pagina
+
+Pagina Producto
+    - promocionar -> meterle un api por type=paginas.Productos & promocionnar=True
+    - Hacer primer saque Miguel
+
+Novedades
+    - checar api por first-published en type=paginas.Productos
+
+Footer
+    - checar Pie de pagina
+    - licencia, redes sociales y contacto con direccion
+
+"""
