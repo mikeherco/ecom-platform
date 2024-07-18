4ccc852d0e31252ffe4f09653f68857a95ae2dea
Miguel Hernández <miguelhdezc12@gmail.com>
2024-07-17 22:06:42 -0600

    DEFAULT_AUTO_FIELD setting added

--- ecom-platform/settings/dev.py
+++ ecom-platform/settings/dev.py
@@ -7,6 +7,7 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-aaa&@&ej+194-qsnt)k01v*plu^_+u_!=)8gn#lrj&%^e=z4u&"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
