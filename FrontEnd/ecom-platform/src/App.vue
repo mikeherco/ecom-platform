646e1c6916aa90ccdb6110953b9eac9a4c60229d
DraCaligari <tatianagarcia0695@hotmail.com>
2024-07-24 16:53:12 -0600

    footer y navbar

--- FrontEnd/ecom-platform/src/App.vue
+++ FrontEnd/ecom-platform/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app>
+  <v-app class="main">
+    <nav-bar/>
     <v-main>
       <router-view />
     </v-main>
+    <main-footer />
   </v-app>
 </template>
 
 <script lang="ts" setup>
   //
+import NavBar from "@/components/NavBar.vue";
+  import MainFooter from "@/components/MainFooter.vue";
 </script>
+<style>
+
+</style>
