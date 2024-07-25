6a94c1275f5fff2061fbd5adbd8a0b172b116906
DraCaligari <tatianagarcia0695@hotmail.com>
2024-07-25 15:55:51 -0600

    Cambios en el navbar y el footer

--- FrontEnd/ecom-platform/src/components/MainFooter.vue
+++ FrontEnd/ecom-platform/src/components/MainFooter.vue
@@ -1,24 +1,25 @@
 <template>
   <v-footer
-    class="bg-indigo-lighten-1 text-center d-flex flex-column"
+    class="bg-indigo-lighten-1 d-flex flex-column"
   >
-    <div>
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        :icon="icon"
-        class="mx-4"
-        variant="text"
-      ></v-btn>
-    </div>
-
-    <div class="pt-0">
-      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    </div>
+          <div class="d-flex">
+        <div class="d-flex flex-row px-2" style="flex: 1 1 50%;">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            :icon="icon"
+            class="mx-4"
+            variant="text"
+          ></v-btn>
+        </div>
+        <div style="flex: 1 1 50%;">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </div>
+      </div>
 
     <v-divider></v-divider>
 
-    <div>
+    <div class="py-3">
       {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
     </div>
   </v-footer>
--- FrontEnd/ecom-platform/src/components/NavBar.vue
+++ FrontEnd/ecom-platform/src/components/NavBar.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-card
-    color="grey-lighten-4"
-    height="200px"
-    rounded="0"
-    flat
-  >
-    <v-toolbar density="compact">
+    <v-toolbar class="py-1 bg-indigo-lighten-1" density="compact">
+<!--
       <v-toolbar-title>Title</v-toolbar-title>
-
+-->
+      <router-link to="/">
+        <v-img src="https://e7.pngegg.com/pngimages/900/287/png-clipart-logo-icon-design-graphics-illustration-company-text.png" width="110px"/>
+      </router-link>
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-account</v-icon>
+        <v-icon>mdi-cart</v-icon>
       </v-btn>
       <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
+        <v-icon>mdi-account</v-icon>
       </v-btn>
     </v-toolbar>
-  </v-card>
 </template>
+<script setup lang="ts">
+</script>
